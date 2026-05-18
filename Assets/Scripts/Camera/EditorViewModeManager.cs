715cbd40c116aa8f63722974e9e67d76cceb8a18
Lim Sowan <87745742+ImperetilliDeus@users.noreply.github.com>
2026-05-18 14:01:47 +0900

    Add editor view mode manager

---
+++ Assets/Scripts/Camera/EditorViewModeManager.cs
@@ -0,0 +1,180 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum EditorViewMode
+{
+    Top = 0,
+    Perspective3D = 1
+}
+
+public sealed class EditorViewModeManager : MonoBehaviour
+{
+    [SerializeField] private EditorViewMode initialViewMode = EditorViewMode.Top;
+    [SerializeField] private Camera topCamera;
+    [SerializeField] private Camera perspectiveCamera;
+    [SerializeField] private Behaviour topCameraManager;
+    [SerializeField] private Behaviour perspectiveCameraManager;
+    [SerializeField] private GameObject[] topViewOnlyRoots;
+    [SerializeField] private Button topButton;
+    [SerializeField] private Button perspectiveButton;
+
+    public EditorViewMode CurrentViewMode { get; private set; } = EditorViewMode.Top;
+
+    public event Action<EditorViewMode> ViewModeChanged;
+
+    private bool buttonsBound;
+
+    private void Awake()
+    {
+        BindButtons();
+        CurrentViewMode = initialViewMode;
+        ApplyViewMode(initialViewMode);
+    }
+
+    private void OnDestroy()
+    {
+        UnbindButtons();
+    }
+
+    public void SetTopView()
+    {
+        SetViewMode(EditorViewMode.Top);
+    }
+
+    public void SetPerspectiveView()
+    {
+        SetViewMode(EditorViewMode.Perspective3D);
+    }
+
+    public void SetViewMode(EditorViewMode mode)
+    {
+        bool changed = CurrentViewMode != mode;
+        CurrentViewMode = mode;
+
+        ApplyViewMode(mode);
+
+        if (changed)
+        {
+            ViewModeChanged?.Invoke(mode);
+        }
+    }
+
+    public void SetReferencesForTests(
+        Camera topCamera,
+        Camera perspectiveCamera,
+        Behaviour topCameraManager,
+        Behaviour perspectiveCameraManager,
+        GameObject[] topViewOnlyRoots,
+        Button topButton,
+        Button perspectiveButton)
+    {
+        UnbindButtons();
+
+        this.topCamera = topCamera;
+        this.perspectiveCamera = perspectiveCamera;
+        this.topCameraManager = topCameraManager;
+        this.perspectiveCameraManager = perspectiveCameraManager;
+        this.topViewOnlyRoots = topViewOnlyRoots;
+        this.topButton = topButton;
+        this.perspectiveButton = perspectiveButton;
+
+        BindButtons();
+    }
+
+    private void BindButtons()
+    {
+        if (buttonsBound)
+        {
+            UnbindButtons();
+        }
+
+        if (topButton != null)
+        {
+            topButton.onClick.AddListener(SetTopView);
+        }
+        else
+        {
+            Debug.LogWarning($"{nameof(EditorViewModeManager)} is missing a top button reference.", this);
+        }
+
+        if (perspectiveButton != null)
+        {
+            perspectiveButton.onClick.AddListener(SetPerspectiveView);
+        }
+        else
+        {
+            Debug.LogWarning($"{nameof(EditorViewModeManager)} is missing a perspective button reference.", this);
+        }
+
+        buttonsBound = true;
+    }
+
+    private void UnbindButtons()
+    {
+        if (topButton != null)
+        {
+            topButton.onClick.RemoveListener(SetTopView);
+        }
+
+        if (perspectiveButton != null)
+        {
+            perspectiveButton.onClick.RemoveListener(SetPerspectiveView);
+        }
+
+        buttonsBound = false;
+    }
+
+    private void ApplyViewMode(EditorViewMode mode)
+    {
+        bool topViewEnabled = mode == EditorViewMode.Top;
+
+        SetEnabled(topCamera, topViewEnabled, nameof(topCamera));
+        SetEnabled(perspectiveCamera, !topViewEnabled, nameof(perspectiveCamera));
+        SetEnabled(topCameraManager, topViewEnabled, nameof(topCameraManager));
+        SetEnabled(perspectiveCameraManager, !topViewEnabled, nameof(perspectiveCameraManager));
+        SetTopViewOnlyRootsActive(topViewEnabled);
+        SetInteractable(topButton, !topViewEnabled, nameof(topButton));
+        SetInteractable(perspectiveButton, topViewEnabled, nameof(perspectiveButton));
+    }
+
+    private void SetTopViewOnlyRootsActive(bool active)
+    {
+        if (topViewOnlyRoots == null)
+        {
+            return;
+        }
+
+        foreach (GameObject root in topViewOnlyRoots)
+        {
+            if (root != null)
+            {
+                root.SetActive(active);
+            }
+        }
+    }
+
+    private void SetEnabled(Behaviour target, bool enabled, string referenceName)
+    {
+        if (target != null)
+        {
+            target.enabled = enabled;
+        }
+        else
+        {
+            Debug.LogWarning($"{nameof(EditorViewModeManager)} is missing a {referenceName} reference.", this);
+        }
+    }
+
+    private void SetInteractable(Button button, bool interactable, string referenceName)
+    {
+        if (button != null)
+        {
+            button.interactable = interactable;
+        }
+        else
+        {
+            Debug.LogWarning($"{nameof(EditorViewModeManager)} is missing a {referenceName} reference.", this);
+        }
+    }
+}
---
+++ Assets/Tests/Editor/EditorViewModeManagerTests.cs
@@ -0,0 +1,245 @@
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EditorViewModeManagerTests
+{
+    private GameObject managerObject;
+    private GameObject topCameraObject;
+    private GameObject perspectiveCameraObject;
+    private GameObject topUiRoot;
+    private GameObject topButtonObject;
+    private GameObject perspectiveButtonObject;
+    private int eventCount;
+
+    [TearDown]
+    public void TearDown()
+    {
+        DestroyObject(perspectiveButtonObject);
+        DestroyObject(topButtonObject);
+        DestroyObject(topUiRoot);
+        DestroyObject(perspectiveCameraObject);
+        DestroyObject(topCameraObject);
+        DestroyObject(managerObject);
+        eventCount = 0;
+    }
+
+    [Test]
+    public void SetPerspectiveView_EnablesPerspectiveCameraAndHidesTopViewRoots()
+    {
+        Component manager = CreateManager(
+            out Camera topCamera,
+            out Camera perspectiveCamera,
+            out Behaviour topManager,
+            out Behaviour perspectiveManager,
+            out Button topButton,
+            out Button perspectiveButton);
+
+        InvokePublic(manager, "SetPerspectiveView");
+
+        Assert.That(GetCurrentViewModeName(manager), Is.EqualTo("Perspective3D"));
+        Assert.That(topCamera.enabled, Is.False);
+        Assert.That(perspectiveCamera.enabled, Is.True);
+        Assert.That(topManager.enabled, Is.False);
+        Assert.That(perspectiveManager.enabled, Is.True);
+        Assert.That(topUiRoot.activeSelf, Is.False);
+        Assert.That(topButton.interactable, Is.True);
+        Assert.That(perspectiveButton.interactable, Is.False);
+    }
+
+    [Test]
+    public void SetTopView_RestoresTopCameraAndTopViewRoots()
+    {
+        Component manager = CreateManager(
+            out Camera topCamera,
+            out Camera perspectiveCamera,
+            out Behaviour topManager,
+            out Behaviour perspectiveManager,
+            out Button topButton,
+            out Button perspectiveButton);
+
+        InvokePublic(manager, "SetPerspectiveView");
+        InvokePublic(manager, "SetTopView");
+
+        Assert.That(GetCurrentViewModeName(manager), Is.EqualTo("Top"));
+        Assert.That(topCamera.enabled, Is.True);
+        Assert.That(perspectiveCamera.enabled, Is.False);
+        Assert.That(topManager.enabled, Is.True);
+        Assert.That(perspectiveManager.enabled, Is.False);
+        Assert.That(topUiRoot.activeSelf, Is.True);
+        Assert.That(topButton.interactable, Is.False);
+        Assert.That(perspectiveButton.interactable, Is.True);
+    }
+
+    [Test]
+    public void SetViewMode_DoesNotRaiseDuplicateEventForSameMode()
+    {
+        Component manager = CreateManager(out _, out _, out _, out _, out _, out _);
+        SubscribeToViewModeChanged(manager);
+
+        InvokeSetViewMode(manager, "Top");
+        InvokeSetViewMode(manager, "Perspective3D");
+        InvokeSetViewMode(manager, "Perspective3D");
+
+        Assert.That(eventCount, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void SetReferencesForTests_RebindsButtonsWithoutDuplicateListeners()
+    {
+        Component manager = CreateManager(
+            out Camera topCamera,
+            out Camera perspectiveCamera,
+            out Behaviour topManager,
+            out Behaviour perspectiveManager,
+            out Button topButton,
+            out Button perspectiveButton);
+        SubscribeToViewModeChanged(manager);
+
+        InvokeSetReferencesForTests(
+            manager,
+            topCamera,
+            perspectiveCamera,
+            topManager,
+            perspectiveManager,
+            new[] { topUiRoot, null },
+            topButton,
+            perspectiveButton);
+
+        perspectiveButton.onClick.Invoke();
+        perspectiveButton.onClick.Invoke();
+        topButton.onClick.Invoke();
+
+        Assert.That(eventCount, Is.EqualTo(2));
+        Assert.That(GetCurrentViewModeName(manager), Is.EqualTo("Top"));
+        Assert.That(topButton.interactable, Is.False);
+        Assert.That(perspectiveButton.interactable, Is.True);
+    }
+
+    private Component CreateManager(
+        out Camera topCamera,
+        out Camera perspectiveCamera,
+        out Behaviour topManager,
+        out Behaviour perspectiveManager,
+        out Button topButton,
+        out Button perspectiveButton)
+    {
+        topCameraObject = new GameObject("TopCamera");
+        topCamera = topCameraObject.AddComponent<Camera>();
+        topManager = topCameraObject.AddComponent<TestViewInputComponent>();
+
+        perspectiveCameraObject = new GameObject("PerspectiveCamera");
+        perspectiveCamera = perspectiveCameraObject.AddComponent<Camera>();
+        perspectiveManager = perspectiveCameraObject.AddComponent<TestViewInputComponent>();
+
+        topUiRoot = new GameObject("TopPlanContent");
+
+        topButtonObject = new GameObject("TopButton");
+        topButton = topButtonObject.AddComponent<Button>();
+        perspectiveButtonObject = new GameObject("PerspectiveButton");
+        perspectiveButton = perspectiveButtonObject.AddComponent<Button>();
+
+        managerObject = new GameObject("EditorViewModeManager");
+        managerObject.SetActive(false);
+        Component manager = managerObject.AddComponent(GetAssemblyType("EditorViewModeManager"));
+        InvokeSetReferencesForTests(
+            manager,
+            topCamera,
+            perspectiveCamera,
+            topManager,
+            perspectiveManager,
+            new[] { topUiRoot },
+            topButton,
+            perspectiveButton);
+        InvokeSetViewMode(manager, "Top");
+        return manager;
+    }
+
+    private void SubscribeToViewModeChanged(Component manager)
+    {
+        EventInfo eventInfo = manager.GetType().GetEvent("ViewModeChanged", BindingFlags.Instance | BindingFlags.Public);
+        Assert.That(eventInfo, Is.Not.Null);
+
+        Type eventType = eventInfo.EventHandlerType;
+        Type parameterType = eventType.GetGenericArguments()[0];
+        ParameterExpression parameter = Expression.Parameter(parameterType, "mode");
+        MethodInfo incrementMethod = GetType().GetMethod(nameof(IncrementEventCount), BindingFlags.Instance | BindingFlags.NonPublic);
+        Delegate handler = Expression.Lambda(eventType, Expression.Call(Expression.Constant(this), incrementMethod), parameter).Compile();
+        eventInfo.AddEventHandler(manager, handler);
+    }
+
+    private void IncrementEventCount()
+    {
+        eventCount++;
+    }
+
+    private static void InvokeSetViewMode(Component manager, string modeName)
+    {
+        MethodInfo method = manager.GetType().GetMethod("SetViewMode", BindingFlags.Instance | BindingFlags.Public);
+        Assert.That(method, Is.Not.Null);
+        object mode = Enum.Parse(GetAssemblyType("EditorViewMode"), modeName);
+        method.Invoke(manager, new[] { mode });
+    }
+
+    private static void InvokeSetReferencesForTests(
+        Component manager,
+        Camera topCamera,
+        Camera perspectiveCamera,
+        Behaviour topManager,
+        Behaviour perspectiveManager,
+        GameObject[] topViewOnlyRoots,
+        Button topButton,
+        Button perspectiveButton)
+    {
+        MethodInfo method = manager.GetType().GetMethod("SetReferencesForTests", BindingFlags.Instance | BindingFlags.Public);
+        Assert.That(method, Is.Not.Null);
+        method.Invoke(
+            manager,
+            new object[]
+            {
+                topCamera,
+                perspectiveCamera,
+                topManager,
+                perspectiveManager,
+                topViewOnlyRoots,
+                topButton,
+                perspectiveButton,
+            });
+    }
+
+    private static void InvokePublic(Component manager, string methodName)
+    {
+        MethodInfo method = manager.GetType().GetMethod(methodName, BindingFlags.Instance | BindingFlags.Public);
+        Assert.That(method, Is.Not.Null);
+        method.Invoke(manager, Array.Empty<object>());
+    }
+
+    private static string GetCurrentViewModeName(Component manager)
+    {
+        PropertyInfo property = manager.GetType().GetProperty("CurrentViewMode", BindingFlags.Instance | BindingFlags.Public);
+        Assert.That(property, Is.Not.Null);
+        return property.GetValue(manager)?.ToString();
+    }
+
+    private static void DestroyObject(UnityEngine.Object target)
+    {
+        if (target != null)
+        {
+            UnityEngine.Object.DestroyImmediate(target);
+        }
+    }
+
+    private static Type GetAssemblyType(string typeName)
+    {
+        Type type = Type.GetType($"{typeName}, Assembly-CSharp");
+        Assert.That(type, Is.Not.Null, $"Expected Assembly-CSharp type {typeName}.");
+        return type;
+    }
+
+    private sealed class TestViewInputComponent : MonoBehaviour
+    {
+    }
+}
